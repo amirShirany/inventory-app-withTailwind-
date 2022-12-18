f95cc8ecf8743378398316b21d179804e9a936d0
Amir.sh <mobilefactory012@gmail.com>
2022-12-18 16:54:53 +0330

    Add : NavBar component and create categories & products

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
+
+const products = [
+  {
+    id: 1,
+    title: "React.js",
+    category: "frontend",
+    createAt: "2021-10-31T15:02:00.411Z",
+  },
+  {
+    id: 2,
+    title: "Node.js",
+    category: "backend",
+    createAt: "2021-10-31T15:03:23.556Z",
+  },
+  {
+    id: 3,
+    title: "Vue.js",
+    category: "frontend",
+    createAt: "2021-11-01T10:47:26.889Z",
+  },
+];
+
+const categories = [
+  {
+    id: 1,
+    title: "frontend",
+    description: "frontend of applications",
+    createAt: "2021-11-01T10:47:26.889Z",
+  },
+  {
+    id: 2,
+    title: "backend",
+    description: "the backend of the applications",
+    createAt: "2021-10-01T10:47:26.889Z",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl bg-red-500 py-2">hi react.js</h1>
+      <div className="bg-slate-800  min-h-screen">
+        <NavBar />
+      </div>
     </div>
   );
 }
