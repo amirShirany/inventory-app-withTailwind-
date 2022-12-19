82b3b1b52c64799392a3db83580331ef3349e4c2
Amir.sh <mobilefactory012@gmail.com>
2022-12-19 13:49:29 +0330

    Add : category form & @tailwindcss/forms

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Category from "./components/Category";
 import NavBar from "./components/NavBar";
 
 const products = [
@@ -39,9 +40,12 @@ const categories = [
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <div className="bg-slate-800  min-h-screen">
         <NavBar />
+        <div className="container max-w-screen-sm mx-auto p-4">
+          <Category />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Category.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+const CategoryForm = () => {
+  const [isShow, setIsShow] = useState(false);
+  return (
+    <section>
+      <div className={`mb-6 ${isShow ? "" : "hidden"}`} id="category-wrapper">
+        <h2 className="text-xl text-slate-300 font-bold mb-2">
+          Add New Category
+        </h2>
+        <form className="bg-slate-700 p-4 rounded-xl flex flex-col gap-y-4">
+          <div>
+            <label
+              for="category-title"
+              className="block mb-1 w-2 text-slate-400"
+            >
+              title
+            </label>
+            <input
+              type="text"
+              name="category-title"
+              id="category-title"
+              className="bg-transparent rounded-xl border border-slate-500 text-slate-400 w-full md:w-auto"
+            />
+          </div>
+          <div>
+            <label
+              for="category-description"
+              className="block mb-1 text-slate-400"
+            >
+              description
+            </label>
+            <textarea
+              type="text"
+              className="bg-transparent rounded-xl border border-slate-500 text-slate-400 w-full"
+            ></textarea>
+          </div>
+          <div className="flex items-center justify-between gap-x-4">
+            <button
+              id="cancel-add-category"
+              className="flex-1 border border-slate-400 text-slate-400 rounded-xl py-2"
+              onClick={(e) => {
+                e.preventDefault();
+                setIsShow(false);
+              }}
+            >
+              Cancel
+            </button>
+            <button
+              id="add-new-category"
+              className="flex-1 bg-slate-500 text-slate-200 rounded-xl py-2"
+            >
+              Add Category
+            </button>
+          </div>
+        </form>
+      </div>
+      <button
+        id="toggle-add-cetegory"
+        className={`text-slate-600 text-lg mb-4 font-medium ${
+          isShow && "hidden"
+        }`}
+        onClick={() => setIsShow((prevState) => !prevState)}
+      >
+        Add new Category?
+      </button>
+    </section>
+  );
+};
+
+export default CategoryForm;
